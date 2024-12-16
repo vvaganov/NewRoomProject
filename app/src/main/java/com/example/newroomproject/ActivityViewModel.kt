54415dadec33d6a2df59817c8bb8aaa4770c8f08
Valery <i@vvaganov.ru>
2024-12-16 16:14:22 +0500

    NR-02 Реализовал переход на дашбоард при загрузке если данные уже есть в бд

--- app/src/main/java/com/example/newroomproject/ActivityViewModel.kt
+++ app/src/main/java/com/example/newroomproject/ActivityViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.newroomproject
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.newroomproject.data.AppDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class ActivityViewModel: ViewModel() {
+class ActivityViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val db = AppDatabase.getInstance(application).userDao()
+
+    suspend fun getRowCount() = withContext(Dispatchers.IO) {
+        db.getRowCountUserParams() > 0
+    }
 }
--- app/src/main/java/com/example/newroomproject/MainActivity.kt
+++ app/src/main/java/com/example/newroomproject/MainActivity.kt
@@ -6,8 +6,14 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.lifecycleScope
 import com.example.newroomproject.databinding.ActivityMainBinding
 import com.example.newroomproject.databinding.FragmentDashBoardBinding
+import com.example.newroomproject.ui.dashBoard.DashBoardFragment
+import com.example.newroomproject.ui.startScreen.StartScreenFragment
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +24,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+
+        lifecycleScope.launch{
+            if (viewModel.getRowCount()){
+                supportFragmentManager.commit{
+                    replace<DashBoardFragment>(R.id.fragment_container_view)
+                }
+            } else{
+                supportFragmentManager.commit{
+                    replace<StartScreenFragment>(R.id.fragment_container_view)
+                }
+            }
+        }
+
+
+
+
     }
 }
